de2f8436436f53955c6d4ff58669f6e4e536e591
vikwal-8 <vikwal-8@student.ltu.se>
2020-11-23 21:01:07 +0100

    aaaa

--- html/index.php
+++ html/index.php
@@ -1,21 +1,21 @@
 <html>
 <body>
 
-
-
-
 <?php
 include "config.php";
-
-if ($_SESSION["uname"] != NULL){
-  //Kompilera iframe i högra hörnet, redan inloggad
-} else {
-  <a href="./login.php">Login / Create account</a>
-  
-}
-
-
 ?>
+
+<div class="topright.css">
+<?php
+if ($_SESSION["uname"] != 0){
+  echo $_SESSION["uname"];
+}
+else(
+  echo "test";
+  )
+?>
+</div>
+
 
 
 </body>
