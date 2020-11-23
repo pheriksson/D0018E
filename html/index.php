ec3483f311df62e984f5f47a9a873bf7f75dba8b
vikwal-8 <vikwal-8@student.ltu.se>
2020-11-23 21:06:23 +0100

    111

--- html/index.php
+++ html/index.php
@@ -1,11 +1,27 @@
 <html>
+<head>
+<style>
+  .topright {
+    position: fixed;
+    right: 5px; top: 5px;
+    width: 200px;
+    height:40px;
+    background-color:lightblue;
+    padding: 5px;
+}
+  .large {
+    width: 2000px; height: 500px;
+    background-color: lightgray;
+}
+</style>
+</head>
 <body>
 
 <?php
 include "config.php";
 ?>
 
-<div rel="stylesheet" href="index.css">
+<div class="topright">
 <?php
 if ($_SESSION["uname"] != 0){
   echo $_SESSION["uname"];
