7c6bb6ba1d9c34bfeb6079df1184227f28a19baf
vikwal-8 <vikwal-8@student.ltu.se>
2020-12-02 23:12:18 +0100

    Update welcome.php

--- html/welcome.php
+++ html/welcome.php
@@ -5,7 +5,7 @@ Welcome <?php
 session_start();
 echo $_SESSION["uname"];
 echo "<br>"."You will be sent to start-page in 5 seconds"
-header('Refresh: 5; Location: index.php');
+header("Refresh: 5; url=index.php");
 ?><br>
 
 
