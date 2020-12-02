4a9c7a7edc55b9041aac63f3479eaf76439ee986
vikwal-8 <vikwal-8@student.ltu.se>
2020-12-02 23:09:47 +0100

    welcome-redirect

--- html/welcome.php
+++ html/welcome.php
@@ -1,9 +1,11 @@
 <html>
 <body>
 
-Welcome <?php 
+Welcome <?php
 session_start();
-echo $_SESSION["uname"]; 
+echo $_SESSION["uname"];
+echo "<br>"."You will be sent to start-page in 5 seconds"
+header('Refresh: 5; Location: index.php');
 ?><br>
 
 
