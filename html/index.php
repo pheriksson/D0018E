809ffdf175c6d2cae3f095e931ef22d8c92ea3be
vikwal-8 <vikwal-8@student.ltu.se>
2020-12-03 01:28:32 +0100

    Update index.php

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
     position: fixed;
     right: 5px; top: 5px;
     width: 200px;
-    height:70px;
+    height:80px;
     background-color:lightgrey;
     padding: 5px;
 }
@@ -37,6 +37,17 @@ include "config.php";
   }
   ?>
 
+  <div style=" position: absolute; bottom: 5px; right: 5px; padding: 5px;">
+    <a href="profile.php">
+      <img src="/pictures/profile.png" alt="Profile" style="width:20px;height:20px;">
+    </a>
+  </div>
+  <div style=" position: absolute; bottom: 5px; left: 5px; padding: 5px;">
+    <a href="/pictures/cart.php">
+      <img src="cart.png" alt="Cart" style="width:20px;height:20px;">
+    </a>
+  </div>
+
   <!--               Display amount                    -->
   <div style=" position: fixed; top: 5px; right: 5px; padding: 5px;">
       <?php
@@ -60,16 +71,7 @@ include "config.php";
       }
       ?>
   </div>
-  <div style=" position: fixed; bottom: 5px; right: 5px; padding: 5px;">
-    <a href="profile.php">
-      <img src="/pictures/profile.png" alt="Profile" style="width:10px;height:10px;">
-    </a>
-  </div>
-  <div style=" position: fixed; bottom: 5px; left: 5px; padding: 5px;">
-    <a href="/pictures/cart.php">
-      <img src="cart.png" alt="Cart" style="width:10px;height:10px;">
-    </a>
-  </div>
+
 </div>
 
 
