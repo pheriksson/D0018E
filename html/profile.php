6c8416e25383dd3695796d72443903d05a971050
vikwal-8 <vikwal-8@student.ltu.se>
2020-12-03 21:46:13 +0100

    Update profile.php

--- html/profile.php
+++ html/profile.php
@@ -17,8 +17,10 @@ $ArrayUser = mysqli_fetch_array($result);
 $fname = $ArrayUser["first_name"];
 $lname = $ArrayUser["last_name"];
 $adress = $ArrayUser["adress"];
-
-
+$city = $ArrayUser["city"];
+$zip_code = $ArrayUser["zip_code"];
+$country = $ArrayUser["country"];
+$card = $ArrayUser["credit_card"];
 
 ?>
 
@@ -44,24 +46,47 @@ $adress = $ArrayUser["adress"];
                         <h2>Update Profile</h2>
                     </div>
 
-                    <p>Fill in the fields you like to update</p>
+                    <p>Fill in the fields you like to update.</p>
+                    </br>
+                    If you would like to change your password, fill in the
+                    password fields. Otherwise leave them blank.
                     <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
                         <div class="form-group">
                             <label>First Name</label>
-                            <input type="text" name="name" class="form-control" value="<?php echo $fname; ?>">
+                            <input type="text" name="fname" class="form-control" value="<?php echo $fname; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group">
                             <label>Last name</label>
-                            <input type="text" name="name" class="form-control" value="<?php echo $lname; ?>">
+                            <input type="text" name="lname" class="form-control" value="<?php echo $lname; ?>">
                             <span class="help-block"></span>
                         </div>
                         <div class="form-group">
                             <label>Address</label>
-                            <input type="text" name="name" class="form-control" value="<?php echo $adress; ?>">
+                            <input type="text" name="adress" class="form-control" value="<?php echo $adress; ?>">
                             <span class="help-block"></span>
                         </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <div class="form-group">
+                            <label>City</label>
+                            <input type="text" name="city" class="form-control" value="<?php echo $city; ?>">
+                            <span class="help-block"></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Zip code</label>
+                            <input type="text" name="zip" class="form-control" value="<?php echo $zip_code; ?>">
+                            <span class="help-block"></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Country</label>
+                            <input type="text" name="country" class="form-control" value="<?php echo $country; ?>">
+                            <span class="help-block"></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Card Number</label>
+                            <input type="text" name="card" class="form-control" value="<?php echo $card; ?>">
+                            <span class="help-block"></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Update">
                         <a href="index.php" class="btn btn-default">Back</a>
                     </form>
                 </div>
