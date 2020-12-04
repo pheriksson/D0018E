b7b289287e944e7eb8956543189620b83eab15da
vikwal-8 <vikwal-8@student.ltu.se>
2020-12-04 01:10:44 +0100

    Update profile.php

--- html/profile.php
+++ html/profile.php
@@ -99,6 +99,7 @@ $card = $ArrayUser["credit_card"];
                         <input type="submit" class="btn btn-primary" value="Update">
                         <a href="index.php" class="btn btn-default">Back</a>
                         <?php
+                          echo "<b style='color:red'>Passwords do not match.</b>";
                             function printPasswordError() {
                               echo "<b style='color:red'>Passwords do not match.</b>";
                             }
