3539831be8975e69ee2be814bfaa76fa003d7edf
vikwal-8 <vikwal-8@student.ltu.se>
2020-12-14 15:13:58 +0100

    aaa

---
+++ html/orderview.php
@@ -0,0 +1,64 @@
+<?php
+include "config.php";
+
+
+
+if($_SESSION["role"] < 1){
+  header("Location:index.php");
+}
+
+
+if (isset($_GET['id']) && $_GET['id'] != "") {
+	$id = $_GET['id'];
+	$results = mysqli_query($conn, "SELECT order_items.*, products.name FROM order_items INNER JOIN products ON products.id = order_items.products_id WHERE user_id = $_SESSION['user_id'] AND order_id = $id");
+  unset($_GET['id']);
+}
+else{
+  echo "Stop try to fiddle with this page";
+}
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+    <style type="text/css">
+    td, th
+    {
+      padding:0 15px;
+    }
+</style>
+</head>
+<body>
+<table>
+	<thead>
+		<tr>
+			<th>Product</th>
+			<th>Amount</th>
+      <th>Feedback</th>
+		</tr>
+	</thead>
+
+	<?php
+  while($products = mysqli_fetch_array($results)){
+    ?>
+		<tr>
+			<td><?php echo $products['name']; ?></td>
+			<td><?php echo $products['amount']; ?></td>
+			<td>
+				<a href="feedback.php?id=<?php echo $products['product_id']; ?>">Rate item</a>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
+
+</body>
+</html>
