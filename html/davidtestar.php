d167ae8bb749fa7a96b9400b5c99ac87fbb55917
xmrsharp <davidwass90@gmail.com>
2020-12-15 22:10:28 +0100

    fail

--- html/davidtestar.php
+++ html/davidtestar.php
@@ -1,13 +1,60 @@
 
 
-    <?php
+Contract|Select|Wrap|Line Numbers
 
-    require_once("db_connect.php");
-    $db = mysqli_connect($host,$user,$password,$db);
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error()); //Verify that we connected.
-        exit();
-    }
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Untitled Document</title>
+    </head>
+    <h1> ADDITION AND SUBRACTION</h1>
+    <body>
+         <table>
+                <form name="nestle" method="post" action="mod_code.php"/>
+                      <tr>
+                          <td>
+                          <label>NAME</label>&nbsp;<th colspan="col"><input name="name" type="text"/></th>
+                          </td>
+                       </tr>
+                       <tr>
+                           <td>
+                           <label>AUTH CODE</label>&nbsp;<th colspan="col"><input name="auth_code" type="text"/></th>
+                           </td>
+                        </tr>
+                         <tr>
+                           <td>
+                           <label>PRODUCT NAME</label>&nbsp;<th colspan="col"><input name="prod_name" type="text"/></th>
+                           </td>
+                        </tr>
+                        <tr>
+                        <td>
+                        <label>NUMBER</label>&nbsp;<th colspan="col"><input name="number" type="text"/></th>
+                        </td>
+                      </tr>
+                      <tr>
+                         <td>
+                         <input name="add" type="submit" value="ADD"/>
+
+
+
+
+                          <input name="subract" type="submit" value="SUBRACT"/>
+                          </td>
+                      </tr>
+
+                  </form>
+                </table>
+
+
+
+
+
+
+    </body>
+    </html>
+    <?php
+include "config.php";
 
     //Try not to use direct user input, if you do, ensure you validate the data.
     ((empty($_POST['prod_name'])) ? die('Product Name is required.') : null); //Verify that $_POST['prod_name'] is not empty.
@@ -23,11 +70,11 @@
         }
 
         $query = "update products SET prod_qty = prod_qty + $add WHERE prod_name = $prod_name";
-        $result = mysqli_query($db,$query);
+        $result = mysqli_query($conn,$query);
 
         if(!$result)
         {
-            exit("quantity not updated ".mysqli_error($db)); //Need to use $db here.
+            exit("quantity not updated ".mysqli_error($conn)); //Need to use $conn here.
         }else{
             echo "$prod_name has been updated";
         }
@@ -43,11 +90,11 @@
 
         $query = "update products SET prod_qty = prod_qty - $subract WHERE prod_name= $prod_name";
 
-        $result = mysqli_query($db,$query);
+        $result = mysqli_query($conn,$query);
 
         if(!$result) //If it is false.
         {
-                exit("quantity not updated ".mysqli_error($db));
+                exit("quantity not updated ".mysqli_error($conn));
         }else{
                 echo "$prod_name has been updated";
         }
