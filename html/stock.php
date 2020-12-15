fa0cb09362368311af7a46feb651170e61e5efcd
xmrsharp <davidwass90@gmail.com>
2020-12-15 21:56:23 +0100

    its alive

--- html/stock.php
+++ html/stock.php
@@ -1,16 +1,12 @@
-<?php
-// Include config file and get user-data.
-echo $_SESSION["role"];
-echo $_SESSION["uname"];
-echo $_SESSION["user_id"];
-echo $_SESSION['user_id'];
 
-//Update profiles fields för att hämta uppdatera data, förvärde databashämtning
-//Submit knapp kör sql $query
-//Duplicera users.php döp om till stock, hämta produkter ist för users
-//Ta  bort knapp, ist för att ta inaktiv på user gör du på orders
-//Sen bara visa lagersaldo typ med fields
-?>
+<!--
+Update profiles fields för att hämta uppdatera data, förvärde databashämtning
+Submit knapp kör sql $query
+Duplicera users.php döp om till stock, hämta produkter ist för users
+Ta  bort knapp, ist för att ta inaktiv på user gör du på orders
+Sen bara visa lagersaldo typ med fields
+-->
+
 
 <?php
 include "config.php";
@@ -38,7 +34,7 @@ $results = mysqli_query($conn, "SELECT * FROM products");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Profile</title>
+    <title>Stock</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -75,6 +71,10 @@ $results = mysqli_query($conn, "SELECT * FROM products");
     ?>
 		<tr>
 			<td><?php echo $product['name']; ?></td>
+      <td><?php echo $product['id']; ?></td>
+      <td><?php echo $product['stock']; ?></td>
+      <td><?php echo $product['cost_unit']; ?></td>
+      <td><?php echo $product['active']; ?></td>
 			<td>
 				<a href="profile.php?edit=<?php echo $user['email']; ?>">Edit</a>
 			</td>
