2915095d50806b6069558a247b511291bb1fd47e
xmrsharp <davidwass90@gmail.com>
2020-12-16 23:00:07 +0100

    users

--- html/orderview.php
+++ html/orderview.php
@@ -16,7 +16,7 @@ else{
 
 
 if($_SESSION["role"] == 1){
-  $results = mysqli_query($conn, "SELECT order_items.*, products.name, products.cost_unit, products.id FROM order_items INNER JOIN products ON products.id=order_items.product_id WHERE user_id = $userid AND order_id = $id");
+  $results = mysqli_query($conn, "SELECT order_items.*, products.name, products.cost_unit, products.id, products.stock FROM order_items INNER JOIN products ON products.id=order_items.product_id WHERE user_id = $userid AND order_id = $id");
 }
 else{
   $results = mysqli_query($conn, "SELECT order_items.*, products.name, products.cost_unit, products.stock FROM order_items INNER JOIN products ON products.id=order_items.product_id WHERE order_id = $id");
